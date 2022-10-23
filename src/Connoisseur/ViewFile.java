63111698d8954d132c0983dce05041ae145bd07d
justinThompson-jp <jtthompson895@gmail.com>
2022-10-23 16:15:13 -0700

    Removed outputting metadata info to console. Now places metadata into array list and then prints the list.

--- src/Connoisseur/ViewFile.java
+++ src/Connoisseur/ViewFile.java
@@ -5,6 +5,7 @@ package Connoisseur;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.io.*;
 
 public class ViewFile {
@@ -12,6 +13,7 @@ public class ViewFile {
 	private static void FileAttributes(){
 		Scanner userIn = new Scanner(System.in);
     	String pathname;
+    	ArrayList<String> mdata = new ArrayList<String>();
         BasicFileAttributes attr;
         
         System.out.println("Insert file path: ");
@@ -21,10 +23,11 @@ public class ViewFile {
         Path path = Paths.get(pathname);
         try {
          attr = Files.readAttributes(path, BasicFileAttributes.class);
-         System.out.println("Creation time: " + attr.creationTime());
-         System.out.println("Last access time: " + attr.lastAccessTime());
-         System.out.println("Last modified time: " + attr.lastModifiedTime());
-         System.out.println("File size: " + attr.size() + " bytes");
+         mdata.add("Creation time: " + attr.creationTime());
+         mdata.add("Last access time: " + attr.lastAccessTime());
+         mdata.add("Last modified time: " + attr.lastModifiedTime());
+         mdata.add("File size: " + attr.size() + " bytes");
+         System.out.println(mdata);
         } catch (IOException e) {
          System.out.println("Invalid file path: " + e.getMessage());
         }
