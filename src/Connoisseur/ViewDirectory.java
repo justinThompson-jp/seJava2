4c442a5b5c0ca601b9d97152d56b3cd43f8ae6f5
justinThompson-jp <jtthompson895@gmail.com>
2022-11-12 22:09:45 -0800

    Added method for getting saved user-specified directory.
    Merge branch 'main' of https://github.com/justinThompson-jp/seJava2
    
    into main

--- src/Connoisseur/ViewDirectory.java
+++ src/Connoisseur/ViewDirectory.java
@@ -7,12 +7,24 @@ package Connoisseur;
  */
 
 //import java.util.Scanner;
-import java.io.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
 public class ViewDirectory {
 	
 	static int dirLength;
 	static String[] pathnames = null; // Creates an array in which we will store the names of files and directories
+	
+	private static final String SP = File.separator;
+	private static final String PROGRAM_NAME = "Connoisseur";
+	private String appDataDir;
+	
 	
 	public void Directory(String _path) {
 
@@ -34,9 +46,21 @@ public class ViewDirectory {
         //}	
         dirLength = pathnames.length;
 	}
+	
+	public String getDefaultDir() throws FileNotFoundException, IOException, ParseException {
+		this.appDataDir = System.getenv("APPDATA") + SP + PROGRAM_NAME;
+		Object obj = new JSONParser().parse(new FileReader(appDataDir + SP + "system-data.json"));
+		
+		JSONObject jo = (JSONObject) obj;
+		
+		String directory = (String) jo.get("default-directory");
+		return directory;
+	}
 /*
-	public static void main(String[] args) {
-		Directory();
+	public static void main(String[] args) throws ParseException, IOException {
+		DefaultDirectory x = new DefaultDirectory();
+		String s = x.getDefaultDir();
+		System.out.println(s);
 	}
 */
 }
