5a5c63f97dc848073c5c16e86b1db9779695088c
JDCrawf <JDCrawford100@gmail.com>
2022-11-29 03:20:01 -0800

    PlaylistManager will now create empty playlist-data file if none exists

--- src/Connoisseur/PlaylistManager.java
+++ src/Connoisseur/PlaylistManager.java
@@ -1,6 +1,10 @@
 package Connoisseur;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -31,8 +35,31 @@ public class PlaylistManager {
 		String playlist_path = appdata_dir + "\\playlist-data.json";
 		playlist_file = new File(playlist_path);
 		
-		// 
+		// first time loading, i.e. playlist-data.json doesn't exist yet
+		if (!playlist_file.exists()) {
+			try {
+				playlist_file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			this.playlist_data = new JSONObject();
+			
+			FileWriter file;
+			try {
+				file = new FileWriter(playlist_path);
+				file.write(playlist_data.toString());
+				file.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println("[Connoisseur] Created playlist data file");
+		} else {
+			// TODO line 102 in FileManager
+		}
+		
+		
 	}
+	
 	
 	// dummy main for testing
 	public static void main(String[] args) {
