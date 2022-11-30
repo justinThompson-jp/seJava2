114c0ad40c85bb590ff868a1633832cd889dabf4
JDCrawf <JDCrawford100@gmail.com>
2022-11-29 21:16:25 -0800

    Can read and create playlist-data.json sort of

--- src/Connoisseur/PlaylistManager.java
+++ src/Connoisseur/PlaylistManager.java
@@ -1,11 +1,12 @@
 package Connoisseur;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -26,11 +27,16 @@ public class PlaylistManager {
 	
 	
 	// Getter method(s)
+	public File getPlaylistFile() {
+		return playlist_file;
+	}
 	
+	public JSONObject getPlaylistData() {
+		return playlist_data;
+	}
 	
 	// Other methods
 	private void init() {
-		
 		// references the playlist json file
 		String playlist_path = appdata_dir + "\\playlist-data.json";
 		playlist_file = new File(playlist_path);
@@ -52,17 +58,48 @@ public class PlaylistManager {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			System.out.println("[Connoisseur] Created playlist data file");
+			System.out.println("[Connoisseur] Created playlist data file.");
+		
+		// the playlist-data.json already exists
 		} else {
-			// TODO line 102 in FileManager
+			try {
+				JSONParser parser = new JSONParser();
+				this.playlist_data = (JSONObject) parser.parse(new FileReader(playlist_path));
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println("[Connoisseur] Loaded playlist data.");
 		}
-		
-		
 	}
 	
+	// 
+	@SuppressWarnings("unchecked")
+	public void buildPlaylist(String _tag) {
+		JSONArray playlist_contents = new JSONArray();
+		ArrayList<String> file_paths = new ArrayList<String>();
+		// TODO search directory by tag to fill out file_paths arraylist
+		
+		// loops through adding each found path to the playlist_contents JSONArray
+		for (int i = 0; i < file_paths.size(); i++) {
+			playlist_contents.add(file_paths.get(i));
+		}
+		
+		playlist_data.put(_tag, playlist_contents);
+		savePlaylist(playlist_data);
+	}
 	
+	public void savePlaylist(JSONObject _playlist) {
+		try {
+			FileWriter file = new FileWriter(playlist_file.getPath());
+			file.write(_playlist.toJSONString());
+			file.close();
+    		System.out.println("[Connoisseur] Saved playlist data file.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("[Connoisseur] Something went wront while saving playlist data.");
+		}
+	}
 	// dummy main for testing
 	public static void main(String[] args) {
-		System.out.println("PlaylistManager Created");
 	}
 }
