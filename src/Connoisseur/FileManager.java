67a25e4ab1a54b9c2f0774dbd01e60bed313b52d
jvallejoromero <jvallejoromero@csustan.edu>
2022-12-06 00:51:55 -0800

    Bug fix
    Made a mistake in which symbol got assigned to what row.

--- src/Connoisseur/FileManager.java
+++ src/Connoisseur/FileManager.java
@@ -43,8 +43,8 @@ public class FileManager {
 	private int filesScanned = 0;
 	
 	private ImageIcon tagsIcon = new ImageIcon("resources/gui/menubar/icons8-pencil-16.png");
-	private ImageIcon fileIcon = new ImageIcon("resources/gui/view/folder.png");
-	private ImageIcon folderIcon = new ImageIcon("resources/gui/view/file.png");
+	private ImageIcon fileIcon = new ImageIcon("resources/gui/view/file.png");
+	private ImageIcon folderIcon = new ImageIcon("resources/gui/view/folder.png");
 
 	public FileManager() {
 		this.init();
